50f8ec47a2e0e9db36748a92115d9544ee917229
Ashutosh Sharma <ashu.the.engg@gmail.com>
2023-02-05 18:28:02 +0530

    navigation changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => (
 
 		<div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
 			<p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-gray-900">
-				Copyright Ⓒ 2022 Dafforworth. All Rights Reserved.
+				Copyright Ⓒ 2023 Dafforworth. All Rights Reserved.
 			</p>
 
 			<div className="flex flex-row md:mt-0 mt-6">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[248px] h-[64px]" />
+      <img src={logo} alt="daffoworth" className="w-[248px] h-[64px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -36,7 +36,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
