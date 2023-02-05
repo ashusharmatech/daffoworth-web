d0027427f0765caaa6c7712ef60b1d0251ed2f75
Ashutosh Sharma <ashu.the.engg@gmail.com>
2023-02-05 18:15:25 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import styles from '../style';
 import { logo } from '../assets';
 import { footerLinks, socialMedia } from '../constants';
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => (
 	<section id="contact" className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
@@ -23,21 +24,13 @@ const Footer = () => (
 		</div>
 
 		<div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-			<p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+			<p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-gray-900">
 				Copyright Ⓒ 2022 Dafforworth. All Rights Reserved.
 			</p>
 
 			<div className="flex flex-row md:mt-0 mt-6">
 				{socialMedia.map((social, index) => (
-					<img
-						key={social.id}
-						src={social.icon}
-						alt={social.id}
-						className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1
-							? 'mr-6'
-							: 'mr-0'}`}
-						onClick={() => window.open(social.link)}
-					/>
+					<SocialIcon className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6': 'mr-0'}`} url={social.link} />
 				))}
 			</div>
 		</div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -97,8 +97,8 @@ export const footerLinks = [
     title: "Useful Links",
     links: [
       {
-        name: "Content",
-        link: "https://www.hoobank.com/content/",
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/showcase/daffoworth/",
       },
       {
         name: "How it Works",
@@ -160,24 +160,14 @@ export const footerLinks = [
 
 export const socialMedia = [
   {
-    id: "social-media-1",
-    icon: instagram,
-    link: "https://www.instagram.com/",
-  },
-  {
-    id: "social-media-2",
-    icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
     id: "social-media-3",
     icon: twitter,
-    link: "https://www.twitter.com/",
+    link: "https://twitter.com/DaffoWorth",
   },
   {
     id: "social-media-4",
     icon: linkedin,
-    link: "https://www.linkedin.com/",
+    link: "https://www.linkedin.com/showcase/daffoworth/"
   },
 ];
 
