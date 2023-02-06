ae2c5cb7062ed5c57922034c65803076e6c5fede
Ashutosh Sharma <ashu.the.engg@gmail.com>
2023-02-07 00:30:44 +0530

    plugin chages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
   mode: "jit",
   theme: {
     extend: {
@@ -23,5 +23,7 @@ module.exports = {
       xl: "1700px",
     },
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 };
